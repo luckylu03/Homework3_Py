5943b7e2a6e6c3fdbd0632a02322c899bf290ac7
Vladyslav Starovierov <starovierov0311@gmail.com>
2019-03-04 22:35:04 +0200

    class work

---
+++ classwork.py
@@ -0,0 +1,47 @@
+# length = int(input("enter length: "))
+# lis = []
+# for index in range(length):
+#     num = int(input("num: "))
+#     lis.append(num)
+# print (max(lis))
+# print (min(lis))
+
+
+
+# length = int(input("enter length: "))
+# lis = [int(input("num: ")) for index in range(length)]
+# print (max(lis))
+# print (min(lis))
+
+#################################################################################
+
+# lis = [index for index in range(10) if index%2 == 0]
+# lis2 = [index for index in range(10) if index%3 == 0]
+# lis3 = [index for index in range(10) if index%2 != 0 and index%3 != 0]
+# print (lis)
+# print (lis2)
+# print (lis3)
+
+#################################################################################
+
+# num = int(input("enter index: "))
+# lis = [index for index in range(num+1)]
+# lis.pop(0)
+# result = 1
+# for i in lis:
+#     result *= i
+# print (result)
+
+#################################################################################
+
+# login = ''
+# info = {'First': 'Vlad123'}
+# while login != info['First']:
+#     login = input("enter login: ")
+# else:
+#     print ("hello Vlad")
+
+#################################################################################
+
+
+
